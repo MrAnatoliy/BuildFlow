153c8f351a5e019c6c67ea911dd8bc75e640ebb5
MrAnatoliy <mr.anatoliy.main@gmail.com>
2025-04-23 17:29:18 +0300

    Added message broker communication for creating user representation in user service database

---
+++ backend/services/auth_service/messages/producer.py
@@ -0,0 +1,26 @@
+import json
+import logging
+import aio_pika
+
+from core.config import settings
+
+logger = logging.getLogger(settings.service_name)
+
+async def publish_event(routing_key: str, payload: dict):
+
+    event_service, event_name = routing_key.split('.', maxsplit=1)
+    logger.info(f'Recieved message publish request : service - {event_service} | event - {event_name}')
+    logger.info(f'Message body : {payload}')
+    connection = await aio_pika.connect_robust(settings.message_broker_url)
+    logger.info(f'Connection with message broker at {settings.message_broker_url} has been established')
+
+    async with connection:
+        channel = await connection.channel()
+        exchange = await channel.declare_exchange("events", aio_pika.ExchangeType.TOPIC)
+
+        logger.info(f'Exchage and channel is ready. Sending message...')
+
+        await exchange.publish(
+            aio_pika.Message(body=json.dumps(payload).encode()),
+            routing_key=routing_key
+        )
